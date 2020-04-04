3b999fe6d82b2244d3fde32fa27aa15364267c50
Yahyagok <ygok17@hotmail.com>
2020-04-04 16:28:22 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,11 +5,13 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    @student=Student.find(params[:id])
+    render :show
   end
 
   def new 
     @student =Student.new
+    render :new
   end 
 
   def create 
